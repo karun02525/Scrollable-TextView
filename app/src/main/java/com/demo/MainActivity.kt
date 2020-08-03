c6e6ba97de68820bd776e1aaae1f37b982e5f3a5
rishi02525 <54019705+rishi02525@users.noreply.github.com>
2020-08-03 14:46:28 +0530

    TextView scrollable

---
+++ app/src/main/java/com/demo/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        tv.movementMethod=ScrollingMovementMethod()
+    }
+}
